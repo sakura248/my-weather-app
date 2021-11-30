7fedf0ce5debd523dffa5068174a3c11ddad9184
Sakura Nishiya <73939499+sakura248@users.noreply.github.com>
2021-11-29 19:45:50 -0800

    tested axios and devided some files

--- src/App.js
+++ src/App.js
@@ -1,57 +1,61 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useContext, createContext} from 'react'
 import MediaQuery from 'react-responsive'
+// import Loader from "react-loader-spinner";
+import axios from 'axios'
 
 import './App.css'
 
 import CurrentInfo from './components/CurrentInfo'
-// import HourlyWeather from './components/HourlyWeather'
+import HourlyWeather from './components/HourlyWeather'
 import CurrentWeatherDetails from './components/CurrentWeatherDetails'
-// import SearchCityForm from './components/SearchCityForm'
+import SearchCityForm from './components/SearchCityForm'
 
-function App() {
+export const GeoContext = createContext()
+
+export function App() {
     const [lat, setLat] = useState([]);
     const [long, setLong] = useState([]);
     const [data, setData] = useState([])
-    // const [city, setCity] = useState("vancouver")
+
     // const [err, setErr] = useState('')
-    
-    // const SearchCity=(event)=>{
-    //   event.preventDefault();
-    //   const newCity = document.querySelector("#city").value
-    //   setCity(newCity)
-    // }
-
     useEffect(() => {
-        const fetchData = async () => {
-            navigator.geolocation.getCurrentPosition(function(position) {
-                setLat(position.coords.latitude);
-                setLong(position.coords.longitude);
-            });
-            
-            await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-            // await fetch(`${process.env.REACT_APP_API_URL}/weather/?q=${city}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-            .then(res => res.json())
-            .then(result => {
-                if(result.cod === '404') {
+      const fetchData = async () => {
+          navigator.geolocation.getCurrentPosition(function(position) {
+              setLat(position.coords.latitude);
+              setLong(position.coords.longitude);
+          });
+          
+          await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+          // await fetch(`${process.env.REACT_APP_API_URL}/weather/?q=${city}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+          .then(res => res.json())
+          .then(result => {
+              if(result.cod === '404') {
 
-                  alert("City Not Found!");
-                }
-                if(result.cod === 200) {
-                  setData(result)
-                }
-            })
-            .catch(error => {
-              console.log(error)
-            })
-        }
+                alert("City Not Found!");
+              }
+              if(result.cod === 200) {
+                setData(result)
+              }
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
     fetchData();
     }, [lat, long]);
-    // }, [city]);
+
 
 
 
   return (
     <div className='App'>
+      {/* <Loader
+        type="Puff"       
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+      /> */}
       <div className="left-side">
         <MediaQuery query="(max-width: 499px)">
           {/* <SearchCityForm SearchCity={SearchCity} className="search-city-info"/> */}
@@ -62,16 +66,20 @@ function App() {
           ): (
             <div>reading error</div>
         )}
-        {/* {(typeof data.main != 'undefined') ? (
-        city && <HourlyWeather city={city} />
-        ): ( */}
-            <div>reading error</div>
-        )}
+        <GeoContext.Provider value={{lat, long}}>
+          {(typeof data.main != 'undefined') ? (
+          data && <HourlyWeather city={data} />
+            ): (
+              <div>reading error</div>
+          )}
+        </GeoContext.Provider>
       </div>
       <div className="right-side">
         <MediaQuery query="(min-width: 500px)">
           <CurrentInfo weatherData={data} className="current-info" />
-          {/* <SearchCityForm SearchCity={SearchCity} className="search-city-info"/> */}
+          <SearchCityForm 
+          // SearchCity={SearchCity}
+          className="search-city-info"/>
         </MediaQuery>
       </div>
     </div>
--- src/components/SearchCityForm.jsx
+++ src/components/SearchCityForm.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SearchCityForm.css'
+import axios from 'axios'
+
 
 
 const SearchCityForm = (props) => {
 
+    const [query, setQuery] = useState("")
+    const [data, setData] = useState([])
+
+    const setQueryHandler =(event)=> {
+        event.preventDefault();
+        const newQuery = document.querySelector("#cityName").value
+        setQuery(newQuery)
+        SearchCity(newQuery)
+    } 
+
+    const SearchCity = (city) => {
+        axios
+            .get(
+                `${process.env.REACT_APP_API_URL}/weather/?q=${
+                    city !== "[object Object]" ? city : query
+                }&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+                )
+        .then(res => {
+            // if(res.cod === '404') {
+            //     alert("City Not Found!");
+            // }
+            if(res.statusText === 'OK') {
+                setData(res.data)
+            }
+            setQuery('')
+            })
+        .catch(error => {
+            console.log(error)
+            })
+    }
+
+
     return (
         <div className="form-wrapper">
-            <form className="search-form" onSubmit={props.SearchCity}>
+            <form className="search-form" onSubmit={setQueryHandler}>
                 <input
                     className="search-input"
                     type='text'
-                    name='city'
-                    id='city'
+                    name='cityName'
+                    id='cityName'
                     placeholder='Enter a City Name'
                 />
             </form>
